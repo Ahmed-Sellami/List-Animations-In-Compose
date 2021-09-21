0cab22193090568affdd478a79c0b75d8ebe13e8
Ahmed Sellami <ahmedsellami526@gmail.com>
2021-09-22 00:48:03 +0100

    Added an offset that defines the minimum value to make the item slide

--- app/src/main/java/com/example/listanimationsincompose/ui/DragToReorder.kt
+++ app/src/main/java/com/example/listanimationsincompose/ui/DragToReorder.kt
@@ -1,6 +1,5 @@
 package com.example.listanimationsincompose.ui
 
-import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.drag
@@ -16,8 +15,8 @@ import com.example.listanimationsincompose.model.ShoesArticle
 import com.example.listanimationsincompose.model.SlideState
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.plus
 import kotlin.math.roundToInt
+import kotlin.math.sign
 
 fun Modifier.dragToReorder(
     shoesArticle: ShoesArticle,
@@ -38,7 +37,8 @@ fun Modifier.dragToReorder(
                 offsetX.stop()
                 offsetY.stop()
 
-                var numberOfSlidedItems = 0
+                val offsetToSlide = itemHeight / 5
+                var numberOfItems = 0
                 var previousNumberOfItems: Int
                 var listOffset = 0
                 // Wait for drag events.
@@ -51,46 +51,33 @@ fun Modifier.dragToReorder(
                         val verticalDragOffset = offsetY.value + change.positionChange().y
                         launch {
                             offsetY.snapTo(verticalDragOffset)
-                            previousNumberOfItems = numberOfSlidedItems
-                            numberOfSlidedItems = if (offsetY.value > 0) {
-                                val numberOfItems = (offsetY.value / itemHeight).toInt()
-                                if (numberOfItems != 0) {
-                                    updateSlidedState(
-                                        shoesArticles[shoesArticles.indexOf(shoesArticle) + numberOfItems],
-                                        SlideState.UP
-                                    )
-                                }
-                                if (previousNumberOfItems > numberOfItems) {
-                                    updateSlidedState(
-                                        shoesArticles[shoesArticles.indexOf(shoesArticle) + previousNumberOfItems],
-                                        SlideState.NONE
-                                    )
-                                }
-                                listOffset = numberOfItems
-                                numberOfItems
-                            } else {
-                                val numberOfItems = (-offsetY.value / itemHeight).toInt()
-                                if (numberOfItems != 0) {
-                                    updateSlidedState(
-                                        shoesArticles[shoesArticles.indexOf(shoesArticle) - numberOfItems],
-                                        SlideState.DOWN
-                                    )
-                                }
-                                if (previousNumberOfItems > numberOfItems) {
-                                    updateSlidedState(
-                                        shoesArticles[shoesArticles.indexOf(shoesArticle) - previousNumberOfItems],
-                                        SlideState.NONE
-                                    )
-                                }
-                                listOffset = -numberOfItems
-                                numberOfItems
+                            previousNumberOfItems = numberOfItems
+                            val offsetSign = offsetY.value.sign.toInt()
+                            numberOfItems = calculateNumberOfSlidedItems(
+                                offsetY.value * offsetSign,
+                                itemHeight,
+                                offsetToSlide,
+                                previousNumberOfItems
+                            )
+                            if (numberOfItems != 0) {
+                                updateSlidedState(
+                                    shoesArticles[shoesArticles.indexOf(shoesArticle) + numberOfItems * offsetSign],
+                                    if (offsetSign == 1) SlideState.UP else SlideState.DOWN
+                                )
                             }
+                            if (previousNumberOfItems > numberOfItems) {
+                                updateSlidedState(
+                                    shoesArticles[shoesArticles.indexOf(shoesArticle) + previousNumberOfItems * offsetSign],
+                                    SlideState.NONE
+                                )
+                            }
+                            listOffset = numberOfItems * offsetSign
                         }
                         // Consume the gesture event, not passed to external
                         change.consumePositionChange()
                     }
                 }
-                if (numberOfSlidedItems == 0) {
+                if (numberOfItems == 0) {
                     launch {
                         offsetX.animateTo(0f)
                     }
@@ -108,4 +95,24 @@ fun Modifier.dragToReorder(
             // Use the animating offset value here.
             IntOffset(offsetX.value.roundToInt(), offsetY.value.roundToInt())
         }
+}
+
+private fun calculateNumberOfSlidedItems(
+    offsetY: Float,
+    itemHeight: Int,
+    offsetToSlide: Int,
+    previousNumberOfItems: Int
+): Int {
+    val numberOfItemsInOffset = (offsetY / itemHeight).toInt()
+    val numberOfItemsPlusOffset = ((offsetY + offsetToSlide) / itemHeight).toInt()
+    val numberOfItemsMinusOffset = ((offsetY - offsetToSlide - 1) / itemHeight).toInt()
+    return if (offsetY - offsetToSlide - 1 < 0) {
+        0
+    } else if (numberOfItemsPlusOffset > numberOfItemsInOffset && numberOfItemsMinusOffset == numberOfItemsInOffset) {
+        numberOfItemsPlusOffset
+    } else if (numberOfItemsMinusOffset < numberOfItemsInOffset && numberOfItemsPlusOffset == numberOfItemsInOffset) {
+        numberOfItemsInOffset
+    } else {
+        previousNumberOfItems
+    }
 }
--- app/src/test/java/com/example/listanimationsincompose/ExampleUnitTest.kt
+++ app/src/test/java/com/example/listanimationsincompose/ExampleUnitTest.kt
@@ -11,7 +11,23 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun sliding_isCorrect() {
+        val offsetY = 10f
+        val itemHeight = 100f
+        val offsetToSlide = 10f
+        val previousNumberOfItems = 1
+        val numberOfItemsInOffset = (offsetY / itemHeight).toInt()
+        val numberOfItemsPlusOffset = ((offsetY + offsetToSlide) / itemHeight).toInt()
+        val numberOfItemsMinusOffset = ((offsetY - offsetToSlide - 1) / itemHeight).toInt()
+        val numberOfItems = if (offsetY - offsetToSlide - 1 < 0) {
+            0
+        } else if (numberOfItemsPlusOffset > numberOfItemsInOffset && numberOfItemsMinusOffset == numberOfItemsInOffset) {
+            numberOfItemsPlusOffset
+        } else if(numberOfItemsMinusOffset < numberOfItemsInOffset && numberOfItemsPlusOffset == numberOfItemsInOffset) {
+            numberOfItemsInOffset
+        } else {
+            previousNumberOfItems
+        }
+        assertEquals(0, numberOfItems)
     }
 }
